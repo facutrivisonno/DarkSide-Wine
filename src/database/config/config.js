66c3d41631e059161318aae439cc1a5600cb43ef
facutrivisonno <facundotrivi10@gmail.com>
2023-11-10 19:04:08 -0300

    configuracion puerto db

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,14 +1,14 @@
 module.exports= {
   "development": {
-    "username": "root",
-    "password": null,
+    "username": "dbDS",
+    "password": 12345678,
     "database": "darkside",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
+    "username": "dbDS",
+    "password": 12345678,
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
